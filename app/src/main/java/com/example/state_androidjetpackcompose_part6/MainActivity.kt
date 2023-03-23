3dde8334c1f3ef9d957a0d1f21ce3ae93de4c639
BatuhanGuven <batuhanguven3806@gmail.com>
2023-03-23 20:59:25 +0100

    state

---
+++ app/src/main/java/com/example/state_androidjetpackcompose_part6/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.example.state_androidjetpackcompose_part6
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.state_androidjetpackcompose_part6.ui.theme.StateAndroidJetpackComposePart6Theme
+import kotlin.random.Random
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ColorBox()
+        }
+    }
+}
+@Composable
+fun ColorBox(){
+    val color = remember{mutableStateOf(Color.Yellow)}
+    Box(modifier = Modifier
+        .background(color.value)
+        .fillMaxSize()
+        .clickable {
+            color.value = Color(
+                Random.nextFloat(),
+                Random.nextFloat(),
+                Random.nextFloat(),
+                1f
+            )
+        }
+    )
+}
